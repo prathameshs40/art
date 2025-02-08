c6827058ba98b31a6e9ec6048018f704c81e2e2b
prathamesh.soundalgekar <prathamesh.soundalgekar@zerovaega.in>
2025-02-08 16:37:25 +0530

    removed backend

--- artbackend/routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const userRoutes = express.Router();
-
-const userController = require("../controllers/userController");
-userRoutes.get("/", userController.userMsg);
-
-module.exports = userRoutes;
